deedf87f62d4dd19a458de8aff92db5f59008de3
CharlesCook1 <charles.cook2209@gmail.com>
2024-11-26 17:10:35 +0100

    page accueil COOK1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import PagePrincipale from './PagePrincipale';
 import PageProfil from './PageProfil';
+import PageAccueil from './PageAccueil';
 
 function App() {
   return (
@@ -10,12 +11,14 @@ function App() {
       <div>
         {/* Barre de navigation */}
         <nav className="navigation">
-          <Link to='/PagePrincipale'>Page Principalel</Link>
+          <Link to='/PageAccueil'>Page Accueil</Link>
+          <Link to='/PagePrincipale'>Page Principale</Link>
           <Link to='/PageProfil'>Profil</Link>
         </nav>
 
         {/* Configuration des routes */}
         <Routes>
+          <Route path='/PageAccueil'element={<PageAccueil />} />
           <Route path='/PagePrincipale'element={<PagePrincipale />} />
           <Route path='/PageProfil' element={<PageProfil />} />
         </Routes>
