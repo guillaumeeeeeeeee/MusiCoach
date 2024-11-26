6de0bd922587d251f05bdd790f9bc3dc24affa35
CharlesCook1 <charles.cook2209@gmail.com>
2024-11-26 17:32:42 +0100

    Routes COOK1

--- frontend/src/PageAccueil.js
+++ frontend/src/PageAccueil.js
@@ -28,8 +28,8 @@ function BoutonInscription({ titre, link }) {
       <div className='entire-page'>
         <img className='logo-accueil' src={Logo} alt="MusiCoach"/>
         <h1 className='sous-titre' >Apprend à jouer d'un instrument de façon ludique depuis chez toi !</h1>
-        <BoutonConnexion titre="Connexion" link="https://www.larousse.fr/dictionnaires/francais/challenge/14488"></BoutonConnexion>
-        <BoutonInscription titre="Inscription" link="https://www.larousse.fr/dictionnaires/francais/challenge/14488"></BoutonInscription>
+        <BoutonConnexion titre="Connexion" link="/PageLogIn"></BoutonConnexion>
+        <BoutonInscription titre="Inscription" link="/PageSignUp"></BoutonInscription>
       </div>
     );
   }
