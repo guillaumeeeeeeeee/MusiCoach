aaad3c4b2d2ed87f6efcc630c3bbead6ef8cbe80
baptisteaudouin <116082439+baptiste7905@users.noreply.github.com>
2025-01-14 15:37:42 +0100

    14/01

--- backend/model/load_crepe.py
+++ backend/model/load_crepe.py
@@ -1,13 +1,14 @@
 import subprocess
 
-# Commande à exécuter
-commande = ["crepe", "/Users/baptisteaudouin/Documents/GitHub/MusiCoach/backend/audios/extrait_dodo.wav", "--step-size", "40"]
+def function_load_crepe(wave_file):
+    # Commande à exécuter
+    commande = ["crepe", wave_file, "--step-size", "40"]
 
-# Exécution de la commande
-try:
-    subprocess.run(commande, check=True)
-    print("Commande exécutée avec succès !")
-except subprocess.CalledProcessError as e:
-    print(f"Erreur lors de l'exécution de la commande : {e}")
+    # Exécution de la commande
+    try:
+        subprocess.run(commande, check=True)
+        print("Commande exécutée avec succès !")
+    except subprocess.CalledProcessError as e:
+        print(f"Erreur lors de l'exécution de la commande : {e}")
 
 
