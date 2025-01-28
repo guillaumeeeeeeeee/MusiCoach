cec1ddefb1b0d6194d797cc71ff19312f8216646
baptisteaudouin <116082439+baptiste7905@users.noreply.github.com>
2025-01-28 14:04:51 +0100

    main_back_front/back

---
+++ backend/model/main_back_back.py
@@ -0,0 +1,19 @@
+import load_crepe as load_crepe
+import read_midi as read_midi
+import analyse as analyse
+import convert.mid_pdf as mid_pdf
+import os
+
+file_name = "extrait_dodo"
+
+
+chemin_extrait = os.path.abspath(os.path.join(os.path.dirname(__file__), f"../audio/{file_name}.wav"))
+
+load_crepe.function_load_crepe(chemin_extrait)
+
+read_midi.midi_generate(analyse.load_analyse(file_name))
+    
+midi_file = "/Users/baptisteaudouin/Documents/GitHub/MusiCoach/partition.mid"
+output_pdf = "temp_files/pdf/extrait_dodo_test_front.pdf"
+    
+mid_pdf.midi_to_pdf(midi_file, output_pdf)
